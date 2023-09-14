0ed131d073c1c07f02696e55c3393c61ff15670b
RumbleJack56 <ujjwalkakar2005@gmail.com>
2023-09-14 10:53:21 +0530

    14 09 2023 10:53

--- Cpp/Base/Pr6/f2.c
+++ Cpp/Base/Pr6/f2.c
@@ -1,1 +1,16 @@
-#include
+#include<stdio.h>
+#include<cstring>
+#define getName(var)   #var
+
+
+int main(){
+    int lambavariablename1,lambavariablename2;
+    printf("Enter 2 numbers: ");
+    scanf("%d %d",&lambavariablename1,&lambavariablename2);
+    printf("%d %d \n",lambavariablename1,lambavariablename2);
+    int maxval = lambavariablename1>lambavariablename2 ? lambavariablename1 : lambavariablename2;
+    char name[50];
+    lambavariablename1>lambavariablename2 ? strcpy(name,getName(lambavariablename1)) : strcpy(name,getName(lambavariablename2));
+    printf("%s is the bigger number with value %d",name,maxval);
+    return 0;
+}
